707ff7ff30cf6b6811a2b1b463a9ba03637de9bc
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2020-02-07 21:12:45 +0530

    [SETUP] Added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BlogModule } from './blog/blog.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
-  imports: [UserModule, BlogModule],
+  imports: [UserModule, BlogModule,
+  TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    port: 3306,
+    username:'root',
+    password: '9896',
+    database: 'blog-app',
+    entities:["dist/**/*.entity{.ts, .js}"],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
